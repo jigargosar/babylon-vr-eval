c77b9971bdf5505f05859ee4b901c5469ab202df
Jigar Gosar <jigar.gosar@gmail.com>
2025-08-26 12:33:00 +0530

    chore: reformat code

--- src/main.js
+++ src/main.js
@@ -118,36 +118,36 @@ async function init() {
 
     // Beat Saber - Controller tracking and saber visualization
     const sabers = {};
-    
+
     // Listen for controller connections
     if (xrHelper.input) {
         xrHelper.input.onControllerAddedObservable.add((controller) => {
             console.log('Controller connected:', controller.uniqueId);
-            
+
             // Create saber for this controller
             const saber = MeshBuilder.CreateCylinder(`saber_${controller.uniqueId}`, {
                 height: 1.5,
                 diameter: 0.05
             }, scene);
-            
+
             // Color based on hand (assuming left=blue, right=red)
             const saberMaterial = new StandardMaterial(`saberMaterial_${controller.uniqueId}`, scene);
             // Default to blue, will be set properly when we detect handedness
             saberMaterial.diffuseColor = new Color3(0, 0.5, 1);
             saberMaterial.emissiveColor = new Color3(0, 0.2, 0.5);
             saber.material = saberMaterial;
-            
+
             // Store saber reference
             sabers[controller.uniqueId] = {
                 controller: controller,
                 saber: saber,
                 material: saberMaterial
             };
-            
+
             // Update saber position each frame
             controller.onMotionControllerInitObservable.add((motionController) => {
                 console.log('Motion controller initialized:', motionController.handness);
-                
+
                 // Set color based on handedness
                 if (motionController.handness === 'left') {
                     saberMaterial.diffuseColor = new Color3(0, 0.5, 1); // Blue
@@ -158,10 +158,10 @@ async function init() {
                 }
             });
         });
-        
+
         xrHelper.input.onControllerRemovedObservable.add((controller) => {
             console.log('Controller disconnected:', controller.uniqueId);
-            
+
             // Clean up saber
             if (sabers[controller.uniqueId]) {
                 sabers[controller.uniqueId].saber.dispose();
@@ -169,20 +169,20 @@ async function init() {
             }
         });
     }
-    
+
     // Update saber positions every frame
     scene.registerBeforeRender(() => {
-        Object.values(sabers).forEach(({ controller, saber }) => {
+        Object.values(sabers).forEach(({controller, saber}) => {
             if (controller.grip) {
                 // Position saber at controller grip position
                 saber.position.copyFrom(controller.grip.position);
-                
+
                 // Rotate saber to point forward like a lightsaber
                 // Apply 90-degree rotation around X-axis to make cylinder point forward
                 const baseRotation = controller.grip.rotationQuaternion.clone();
                 const forwardRotation = Quaternion.FromEulerAngles(Math.PI / 2, 0, 0);
                 saber.rotationQuaternion = baseRotation.multiply(forwardRotation);
-                
+
                 // Offset saber forward from controller grip
                 const forward = new Vector3(0, 0.75, 0); // Move along cylinder's length
                 forward.rotateByQuaternionToRef(saber.rotationQuaternion, forward);
